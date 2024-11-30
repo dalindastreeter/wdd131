178d89765ae335be42e4fb9dd079a4862a8aed5e
dalindastreeter <str22002@byui.edu>
2024-11-29 23:11:19 -0600

    inicial commit

---
+++ week02/scrips/temple.js
@@ -0,0 +1,7 @@
+const hamburger = document.quarySelector('nav');
+const navList = document.querySelector('#menu');
+
+hamburger.addEventListener('click', () => {
+    mainmav.classList.toggle('active');
+    hambutton.classList.toggle('active');
+});
