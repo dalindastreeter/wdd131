513d7b5b1a466960d5032fae3f23ca5c3bc32bbc
dalindastreeter <str22002@byui.edu>
2024-11-30 12:15:37 -0600

    updated script for burger menu

--- week02/scrips/temple.js
+++
@@ -1,7 +0,0 @@
-const hamburger = document.quarySelector('nav');
-const navList = document.querySelector('#menu');
-
-hamburger.addEventListener('click', () => {
-    mainmav.classList.toggle('active');
-    hambutton.classList.toggle('active');
-});
